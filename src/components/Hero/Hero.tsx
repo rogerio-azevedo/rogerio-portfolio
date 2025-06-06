1fb20557983a8c3ce0e2aebf2794d5dd0c2f54a4
rfazevedo <rogerio@rogerioazevedo.net>
2025-06-06 18:03:44 -0400

    feat: create Hero section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Spotlight } from "../ui/Spotlight";
+import { GridBackground } from "../ui/GridBackground";
+import { TextGenerateEffect } from "../ui/TextGenerateEffect";
+import { MagicButton } from "../ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { GridGlobe } from "../ui/GridGlobe";
+
+export const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vh]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vh]" fill="blue" />
+      </div>
+      <GridBackground />
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </p>
+
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Rogério Azevedo, a React/Next.js Developer based in
+            Brazil.
+          </p>
+          <a href="#contact">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
